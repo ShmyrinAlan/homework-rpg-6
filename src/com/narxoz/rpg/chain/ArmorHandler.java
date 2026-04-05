79d0d3301f970eaf408a6262ae5f699235c18092
Alan <alanshmyrinsource@gmail.com>
2026-04-05 19:42:14 +0500

    feat: complete handle for ArmorHandler by todos

--- src/com/narxoz/rpg/chain/ArmorHandler.java
+++ src/com/narxoz/rpg/chain/ArmorHandler.java
@@ -14,5 +14,7 @@ public class ArmorHandler extends DefenseHandler {
         // TODO: Subtract armorValue from incomingDamage; clamp the result to a minimum of 0.
         // TODO: Print an armor message showing how much was absorbed.
         // TODO: Pass the remaining damage to the next handler.
+        System.out.println((incomingDamage-armorValue > 0 ? armorValue : incomingDamage) + " armor was absorbed");
+        passToNext(Math.max(incomingDamage-armorValue, 0), target);
     }
 }
