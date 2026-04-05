94125f2c869311bbf7618c1b76b65299347c8554
Alan <alanshmyrinsource@gmail.com>
2026-04-05 19:54:21 +0500

    feat: complete handle for BlockHandler by todos

--- src/com/narxoz/rpg/chain/BlockHandler.java
+++ src/com/narxoz/rpg/chain/BlockHandler.java
@@ -6,6 +6,7 @@ public class BlockHandler extends DefenseHandler {
     private final double blockPercent;
 
     public BlockHandler(double blockPercent) {
+        if(blockPercent > 1d || blockPercent < 0d) throw new IllegalArgumentException("block percent can not be bigger than 1 or lower than 0");
         this.blockPercent = blockPercent;
     }
 
@@ -16,5 +17,8 @@ public class BlockHandler extends DefenseHandler {
         // TODO: Print a block message showing how much was blocked.
         // TODO: Always pass the remainder to the next handler (block reduces but never stops the chain).
         // Design question: what should happen if the remainder reaches 0 or below?
+        int blocked = (int)Math.round(incomingDamage*blockPercent);
+        System.out.println(blocked + " damage was blocked");
+        passToNext(incomingDamage-blocked, target);
     }
 }
